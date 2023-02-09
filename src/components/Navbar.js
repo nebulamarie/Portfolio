e5caea1dbb7d3768ce0dc227ad73847675d3f212
nebulamarie <mepps1115@gmail.com>
2023-02-08 20:39:08 -0500

    portfolio project update

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+export default function Navbar() {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    return (
+        <ul className="nav">
+            <li>
+                <Link to={'/'}>Home</Link>
+            </li>
+            <li>
+                <Link to={'/about'}>About</Link>
+            </li>
+            <li>
+                 <Link to={'/work'}>Work</Link>
+            </li>
+            <li>
+                <Link to={'/projects'}>Projects</Link>
+            </li>
+            <li>
+                <Link to={'/contact'}>Contact</Link>
+            </li>
+            <li>
+                <input
+                placeholder="enter email"
+                onChange={ (e) =>{
+                    setSearchQuery(e.target.value);
+                    console.log(e.target.value);
+                }}
+                Message ="Message:"
+                onClick={ (e) =>{
+                    setSearchQuery(e.target.value);
+                    console.log(e.target.value);
+                }}
+                />
+                <Link to ={`/user/${searchQuery}`}>Search</Link>
+            </li>
+        </ul>
+    );   
+}
